17d43a473389c9627e4a7e3884b27b9961f4fb1c
Prashun-Mishra <prashunmishra49@gmail.com>
2026-01-29 18:08:56 +0530

    Fix consultation form z-index and add grid.svg

--- components/consultation-form.tsx
+++ components/consultation-form.tsx
@@ -82,7 +82,7 @@ export default function ConsultationForm({ isOpen, onClose, serviceType = 'Gener
                     />
 
                     {/* Modal */}
-                    <div className="fixed inset-0 z-50 flex items-center justify-center p-4 pointer-events-none">
+                    <div className="fixed inset-0 z-[101] flex items-center justify-center p-4 pointer-events-none">
                         <motion.div
                             initial={{ opacity: 0, scale: 0.9, y: 20 }}
                             animate={{ opacity: 1, scale: 1, y: 0 }}
